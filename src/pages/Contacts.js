c19d6a77f65cd7a4ab0f3f05c5d1e7e73fad5298
Sara Løkken <saraloekken@gmail.com>
2020-10-14 23:21:52 +0200

    Adding function
    Missing active status, but tbc.

---
+++ src/pages/Contacts.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { useDataQuery } from '@dhis2/app-runtime'
+import { Menu, MenuItem } from '@dhis2/ui'
+import { Table, TableHead, TableRowHead, TableCellHead, TableBody, TableRow, TableCell } from '@dhis2/ui'
+
+
+class Contacts extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+    
+        
+        
+        
+        return (
+        
+            
+            <h2>Contacts</h2>
+        
+        
+        
+        )   
+    }
+}
+export { Contacts }
+
+
---
+++ src/pages/Tracker.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { useDataQuery } from '@dhis2/app-runtime'
+import { Menu, MenuItem } from '@dhis2/ui'
+import { Table, TableHead, TableRowHead, TableCellHead, TableBody, TableRow, TableCell } from '@dhis2/ui'
+
+
+class Tracker extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+    
+        
+        
+        
+        return (
+        
+            
+            <h2>Tracker</h2>
+        
+        
+        
+        )   
+    }
+}
+export { Tracker }
+
