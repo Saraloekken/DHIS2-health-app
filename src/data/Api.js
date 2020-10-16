5408a8d455cb49806be36b1d87fc3358c2198261
Vilde <vilderhu@student.matnat.uio.no>
2020-10-16 12:47:33 +0200

    ContactsAPI

--- src/data/Api.js
+++ src/data/Api.js
@@ -2,22 +2,30 @@ import React from 'react'
 import { useDataQuery } from '@dhis2/app-runtime'
 
 const query = {
-    trackedEntityInstances: {
-        resource: 'trackedEntityInstances',
-            params: {
-                ou: 'EwEP9IhOwuw', 
-                program: 'uYjxkTbwRNf',
-                fields: ['trackedEntityInstance', 'attributes', 'lastUpdated']
-            },
+    IndexCases: {
+        resource: "trackedEntityInstances",
+        params: {
+          ou: 'EwEP9IhOwuw',
+          program: 'uYjxkTbwRNf',
+          fields: ["trackedEntityInstance", "attributes", "lastUpdated"],
+        }
     },
+    Contacts: {
+      resource: "trackedEntityInstances",
+      params: {
+        ou: 'EwEP9IhOwuw',
+        program: 'DM9n1bUw8W8',
+        fields: ["trackedEntityInstance", "attributes", "lastUpdated"],
+      }
+  }
 }
 
 function findValue (attributes, valueCode) {    
     return (attributes.find((item) => item.code === valueCode) ? attributes.find((item) => item.code === valueCode).value:'')
 }
 
-const IndexCasesApi = () => {
-    const { loading, error, data } = useDataQuery(query)
+const IndexCasesApi = (props) => {
+    const { loading, error, data } = useDataQuery(query) 
     
     return (
       <div>
@@ -25,7 +33,7 @@ const IndexCasesApi = () => {
         {error && <span>{`ERROR: ${error.message}`}</span>}
         {data && (
           <pre>
-              {data.trackedEntityInstances.trackedEntityInstances.map(({ trackedEntityInstance, attributes, lastUpdated}) => (
+              {data.IndexCases.trackedEntityInstances.map(({ trackedEntityInstance, attributes, lastUpdated}) => (
                 <ul key={trackedEntityInstance}>
                     {findValue(attributes, "first_name") + ' ' + findValue(attributes, "surname") + ' ' + findValue(attributes, "patinfo_ageonset") + ' ' + lastUpdated.substring(0,10)} 
                 </ul>
@@ -36,4 +44,25 @@ const IndexCasesApi = () => {
     )
   };
 
-export { IndexCasesApi };
+  const ContactsApi = (props) => {
+    const { loading, error, data } = useDataQuery(query) 
+    
+    return (
+      <div>
+        {loading && <span>...</span>}
+        {error && <span>{`ERROR: ${error.message}`}</span>}
+        {data && (
+          <pre>
+              {data.Contacts.trackedEntityInstances.map(({ trackedEntityInstance, attributes, lastUpdated}) => (
+                <ul key={trackedEntityInstance}>
+                    {findValue(attributes, "first_name") + ' ' + findValue(attributes, "surname") + ' ' + findValue(attributes, "patinfo_ageonset") + ' ' + lastUpdated.substring(0,10)} 
+                </ul>
+            ))}  
+          </pre>
+        )}
+      </div>
+    )
+  };
+
+
+export { IndexCasesApi, ContactsApi };
