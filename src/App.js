963532e03a81261ea58a823eebf767173735c70e
Sara Løkken <saraloekken@gmail.com>
2020-10-21 20:30:32 +0200

    selectors
    hiding selectors when "custom" is not chosen

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import { Relations } from "./pages/Relations.jsx";
 
 const MyApp = () => {
 
+//To statevariabler som oppdateres sammen, kunne vært slått sammen til èn som oppdatererer begge?
+//Refresher innholdet to ganger fordi den tror staten endrer seg to ganger, mens den bare endrer seg en?
+
 const [page, setPage] = useState(<IndexCases/>)
 const [active, setActive] = useState('indexcases')
     
@@ -62,3 +65,5 @@ function relations(){
 }
 
 export default MyApp
+
+
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { SingleSelectField, SingleSelectOption } from '@dhis2/ui';
 import styles from "../App.module.css";
 import { DatePicker } from "../components/DatePicker.jsx";
 
 
-class Filters extends React.Component {
-    render() {
+const Filters = () => { 
+
+    const [enabled, setEnabled] = useState(false)
+    const [interval, setInterval] = useState('1')
+
+        function intervalChange (select){
+            setInterval(select.selected)
+                
+            if(select.selected==5){
+                setEnabled(true)    
+            } else {
+                setEnabled(false)
+            }
+        }
 
         return (
 
@@ -25,7 +37,7 @@ class Filters extends React.Component {
                     />
                 </SingleSelectField>
 
-                <SingleSelectField inputWidth="200px" label="Time interval" selected="1">
+                <SingleSelectField inputWidth="200px" label="Time interval" selected={interval} onChange={intervalChange}>
                     <SingleSelectOption
                         label="Today"
                         value="1"
@@ -40,17 +52,25 @@ class Filters extends React.Component {
                     />
                     <SingleSelectOption
                         label="This month"
-                        value="3"
+                        value="4"
                     />
+
+                    <SingleSelectOption
+                        label="Custom"
+                        value="5"
+                    />
+                    
                 </SingleSelectField>
                 
-                <DatePicker label="From" />
-                <DatePicker label="To" />
+                
+                <DatePicker label="From" disabled={enabled==false}/> 
+                <DatePicker label="To" disabled={enabled==false}/>
+                
                 
             </div>
 
         )
     }
-}
+
 
 export { Filters }
