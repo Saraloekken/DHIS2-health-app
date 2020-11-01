6a60c6632f5bd1be92a52e2a38d77d98706752a5
Sara Løkken <saraloekken@gmail.com>
2020-11-01 19:40:39 +0100

    Table length fix

--- src/data/Api.js
+++ src/data/Api.js
@@ -120,7 +120,7 @@ function findStatus(item) {
 
 const IndexCasesApi = (props) => {
 
-  const [tableLength, setTableLength] = useState(0);
+  let tableLength = 0;
 
   useEffect(() => {
     props.setTaskCount(tableLength);
@@ -145,7 +145,7 @@ const IndexCasesApi = (props) => {
   const indexCases = data.IndexCases.trackedEntityInstances //endre fra return til const (mellomlagre verdien, istedet for å returnere den direkte)
     .filter((item) => filterTable(item.enrollments[0], props.from, props.to));
 
-  setTableLength(indexCases.length);
+  tableLength = indexCases.length;
 
   return indexCases.map(({ trackedEntityInstance, attributes, lastUpdated, enrollments }) => (
     <TableRow>
@@ -193,6 +193,12 @@ const ContactsApi = (props) => {
   const { loading, error, data } = useDataQuery(query);
   const { baseUrl } = useConfig();
 
+  let tableLength = 0;
+
+  useEffect(() => {
+    props.setTaskCount(tableLength);
+  });
+
   if (error) {
     return <p>{`ERROR: ${error.message}`}</p>;
   }
@@ -200,52 +206,59 @@ const ContactsApi = (props) => {
     return <CircularLoader />;
   }
 
-  return data.Contacts.trackedEntityInstances
+  const contacts = data.Contacts.trackedEntityInstances
     .filter((item) =>
       props.tei
         ? filterTableRelationship(item, props.relObject, props.tei)
         : filterTable(item.enrollments[0], props.from, props.to)
     )
-    .map(({ trackedEntityInstance, attributes, lastUpdated, enrollments }) => (
-      <TableRow>
-        <TableCell>{findValue(attributes, "first_name")}</TableCell>
-        <TableCell>{findValue(attributes, "surname")}</TableCell>
-        <TableCell>
-          {enrollments[0]
-            ? enrollments[0].incidentDate.substring(0, 10)
-            : "None"}
-        </TableCell>
-        <TableCell>{lastUpdated.substring(0, 10)}</TableCell>
-        <TableCell>{findValue(attributes, "patinfo_ageonset")}</TableCell>
-        <TableCell>{findValue(attributes, "phone_local")}</TableCell>
-        <TableCell>
-          <Tag
-            dataTest="dhis2-uicore-tag"
-            neutral={findStatus(enrollments[0]) === "SCHEDULE" ? true : false}
-            positive={findStatus(enrollments[0]) === "ACTIVE" ? true : false}
-            default={findStatus(enrollments[0]) === "VISITED" ? true : false}
-            negative={findStatus(enrollments[0]) === "OVERDUE" ? true : false}
-          >
-            {findStatus(enrollments[0])}
-          </Tag>
-        </TableCell>
-        <TableCell>{findDueDate(enrollments[0])}</TableCell>
-        <TableCell dataTest="dhis2-uicore-tablecell" dense>
-          <Button
-            dataTest="dhis2-uicore-button"
-            name="Primary button"
-            onClick={() =>
-              (window.location = `${baseUrl}/dhis-web-tracker-capture/index.html#/dashboard?tei=${trackedEntityInstance}&program=DM9n1bUw8W8&ou=EwEP9IhOwuw`)
-            }
-            primary
-            type="button"
-            value="default"
-          >
-            Track Entity
+
+
+
+  tableLength = contacts.length;
+
+
+
+  return contacts.map(({ trackedEntityInstance, attributes, lastUpdated, enrollments }) => (
+    <TableRow>
+      <TableCell>{findValue(attributes, "first_name")}</TableCell>
+      <TableCell>{findValue(attributes, "surname")}</TableCell>
+      <TableCell>
+        {enrollments[0]
+          ? enrollments[0].incidentDate.substring(0, 10)
+          : "None"}
+      </TableCell>
+      <TableCell>{lastUpdated.substring(0, 10)}</TableCell>
+      <TableCell>{findValue(attributes, "patinfo_ageonset")}</TableCell>
+      <TableCell>{findValue(attributes, "phone_local")}</TableCell>
+      <TableCell>
+        <Tag
+          dataTest="dhis2-uicore-tag"
+          neutral={findStatus(enrollments[0]) === "SCHEDULE" ? true : false}
+          positive={findStatus(enrollments[0]) === "ACTIVE" ? true : false}
+          default={findStatus(enrollments[0]) === "VISITED" ? true : false}
+          negative={findStatus(enrollments[0]) === "OVERDUE" ? true : false}
+        >
+          {findStatus(enrollments[0])}
+        </Tag>
+      </TableCell>
+      <TableCell>{findDueDate(enrollments[0])}</TableCell>
+      <TableCell dataTest="dhis2-uicore-tablecell" dense>
+        <Button
+          dataTest="dhis2-uicore-button"
+          name="Primary button"
+          onClick={() =>
+            (window.location = `${baseUrl}/dhis-web-tracker-capture/index.html#/dashboard?tei=${trackedEntityInstance}&program=DM9n1bUw8W8&ou=EwEP9IhOwuw`)
+          }
+          primary
+          type="button"
+          value="default"
+        >
+          Track Entity
           </Button>
-        </TableCell>
-      </TableRow>
-    ));
+      </TableCell>
+    </TableRow>
+  ));
 };
 
 /*
@@ -295,6 +308,12 @@ const RelationsApi = (props) => {
   const { loading, error, data } = useDataQuery(query);
   const { baseUrl } = useConfig();
 
+  let tableLength = 0;
+
+  useEffect(() => {
+    props.setTaskCount(tableLength);
+  });
+
   if (error) {
     return <p>{`ERROR: ${error.message}`}</p>;
   }
@@ -302,96 +321,100 @@ const RelationsApi = (props) => {
     return <CircularLoader />;
   }
 
-  return data.Relations.trackedEntityInstances
+
+
+  const relations = data.Relations.trackedEntityInstances
     .filter((item) => filterTable(item.enrollments[0], props.from, props.to))
-    .map(
-      ({
-        trackedEntityInstance,
-        attributes,
-        lastUpdated,
-        enrollments,
-        relationships,
-      }) => (
-          <TableRow>
-            <TableCell>{findValue(attributes, "first_name")}</TableCell>
-            <TableCell>{findValue(attributes, "surname")}</TableCell>
-            <TableCell>
-              {enrollments[0]
-                ? enrollments[0].incidentDate.substring(0, 10)
-                : "None"}
-            </TableCell>
-            <TableCell>{lastUpdated.substring(0, 10)}</TableCell>
-            <TableCell>{findValue(attributes, "patinfo_ageonset")}</TableCell>
-            <TableCell>{findValue(attributes, "phone_local")}</TableCell>
-            <TableCell>
-              <Tag
-                dataTest="dhis2-uicore-tag"
-                neutral={findStatus(enrollments[0]) === "SCHEDULE" ? true : false}
-                positive={findStatus(enrollments[0]) === "ACTIVE" ? true : false}
-                default={findStatus(enrollments[0]) === "VISITED" ? true : false}
-                negative={findStatus(enrollments[0]) === "OVERDUE" ? true : false}
-              >
-                {findStatus(enrollments[0])}
-              </Tag>
-            </TableCell>{" "}
-            <TableCell>{findDueDate(enrollments[0])}</TableCell>
-            <TableCell dataTest="dhis2-uicore-tablecell" dense>
-              <ModalContacts
-                toggle={(show) => <Button onClick={show}> View contacts </Button>}
-                content={(hide) => (
-                  <Modal dataTest="dhis2-uicore-modal" large position="middle">
-                    <ModalTitle dataTest="dhis2-uicore-modaltitle">
-                      Overview of Contacts (X)
+  tableLength = relations.length;
+
+  return relations.map(
+    ({
+      trackedEntityInstance,
+      attributes,
+      lastUpdated,
+      enrollments,
+      relationships,
+    }) => (
+        <TableRow>
+          <TableCell>{findValue(attributes, "first_name")}</TableCell>
+          <TableCell>{findValue(attributes, "surname")}</TableCell>
+          <TableCell>
+            {enrollments[0]
+              ? enrollments[0].incidentDate.substring(0, 10)
+              : "None"}
+          </TableCell>
+          <TableCell>{lastUpdated.substring(0, 10)}</TableCell>
+          <TableCell>{findValue(attributes, "patinfo_ageonset")}</TableCell>
+          <TableCell>{findValue(attributes, "phone_local")}</TableCell>
+          <TableCell>
+            <Tag
+              dataTest="dhis2-uicore-tag"
+              neutral={findStatus(enrollments[0]) === "SCHEDULE" ? true : false}
+              positive={findStatus(enrollments[0]) === "ACTIVE" ? true : false}
+              default={findStatus(enrollments[0]) === "VISITED" ? true : false}
+              negative={findStatus(enrollments[0]) === "OVERDUE" ? true : false}
+            >
+              {findStatus(enrollments[0])}
+            </Tag>
+          </TableCell>{" "}
+          <TableCell>{findDueDate(enrollments[0])}</TableCell>
+          <TableCell dataTest="dhis2-uicore-tablecell" dense>
+            <ModalContacts
+              toggle={(show) => <Button onClick={show}> View contacts </Button>}
+              content={(hide) => (
+                <Modal dataTest="dhis2-uicore-modal" large position="middle">
+                  <ModalTitle dataTest="dhis2-uicore-modaltitle">
+                    Overview of Contacts (X)
                   </ModalTitle>
-                    <ModalContent dataTest="dhis2-uicore-modalcontent">
-                      <DataTable
-                        headlines={[
-                          "First name",
-                          "Surname",
-                          "Incident date",
-                          "Last updated",
-                          "Age",
-                          "Phone",
-                          "Status",
-                          "Due date",
-                          "Tracker Capture",
-                        ]}
-                        api={
-                          <ContactsApi
-                            from={props.from}
-                            to={props.to}
-                            tei={trackedEntityInstance}
-                            relObject={relationships}
-                          />
-                        }
-                      />
-                    </ModalContent>
-                    <ModalActions>
-                      <ButtonStrip>
-                        <Button onClick={hide}>Close</Button>
-                      </ButtonStrip>
-                    </ModalActions>
-                  </Modal>
-                )}
-              />
-            </TableCell>
-            <TableCell dataTest="dhis2-uicore-tablecell" dense>
-              <Button
-                dataTest="dhis2-uicore-button"
-                name="Primary button"
-                onClick={() =>
-                  (window.location = `${baseUrl}/dhis-web-tracker-capture/index.html#/dashboard?tei=${trackedEntityInstance}&program=uYjxkTbwRNf&ou=EwEP9IhOwuw`)
-                }
-                primary
-                type="button"
-                value="default"
-              >
-                Track Entity
+                  <ModalContent dataTest="dhis2-uicore-modalcontent">
+                    <DataTable
+                      headlines={[
+                        "First name",
+                        "Surname",
+                        "Incident date",
+                        "Last updated",
+                        "Age",
+                        "Phone",
+                        "Status",
+                        "Due date",
+                        "Tracker Capture",
+                      ]}
+                      api={
+                        <ContactsApi
+                          from={props.from}
+                          to={props.to}
+                          tei={trackedEntityInstance}
+                          relObject={relationships}
+                        />
+                      }
+                    />
+                  </ModalContent>
+                  <ModalActions>
+                    <ButtonStrip>
+                      <Button onClick={hide}>Close</Button>
+                    </ButtonStrip>
+                  </ModalActions>
+                </Modal>
+              )}
+            />
+          </TableCell>
+          <TableCell dataTest="dhis2-uicore-tablecell" dense>
+            <Button
+              dataTest="dhis2-uicore-button"
+              name="Primary button"
+              onClick={() =>
+                (window.location = `${baseUrl}/dhis-web-tracker-capture/index.html#/dashboard?tei=${trackedEntityInstance}&program=uYjxkTbwRNf&ou=EwEP9IhOwuw`)
+              }
+              primary
+              type="button"
+              value="default"
+            >
+              Track Entity
             </Button>
-            </TableCell>
-          </TableRow>
-        )
-    );
+          </TableCell>
+        </TableRow>
+      )
+  );
 };
 
 export { IndexCasesApi, ContactsApi, RelationsApi };
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { WelcomeBox } from "../components/WelcomeBox.jsx"; 
+import { WelcomeBox } from "../components/WelcomeBox.jsx";
 import { DataTable } from "../components/EntityDataTable.jsx";
 import { Filters } from "../components/Filters.jsx";
 import { ContactsApi } from "../data/Api";
@@ -9,14 +9,16 @@ import getDaysForwardDate from "../components/Filters.jsx";
 const Contacts = () => {
   const [from, setFrom] = useState(getDaysForwardDate(0));
   const [to, setTo] = useState(getDaysForwardDate(0));
+  const [taskCount, setTaskCount] = useState(0);
+  const [dayDescription, setDayDescription] = useState('today');
 
   return (
     <div className={styles.container}>
       <h2>Overview of Contacts</h2>
       <div>
         <div className={styles.topbar}>
-           <Filters setFrom={setFrom} setTo={setTo} />
-          <WelcomeBox />
+          <Filters setFrom={setFrom} setTo={setTo} setDayDescription={setDayDescription} />
+          <WelcomeBox taskCount={taskCount} dayDescription={dayDescription} />
         </div>
 
         <DataTable
@@ -31,7 +33,7 @@ const Contacts = () => {
             "Due date",
             "Tracker Capture",
           ]}
-          api={<ContactsApi from={from} to={to}/>}
+          api={<ContactsApi from={from} to={to} setTaskCount={setTaskCount} />}
         />
       </div>
     </div>
