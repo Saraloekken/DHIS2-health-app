9d0bd3e48dda672df5783fedaba0beeeb1ac071b
unknown <sandranhthy@hotmail.com>
2020-11-05 12:20:47 +0100

    changes to styling in modal

--- src/components/ModalContacts.jsx
+++ src/components/ModalContacts.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
+{ /*Heavily influenced by https://wecodetheweb.com/2019/03/02/easy-modals-with-react-hooks/?fbclid=IwAR3dwfDbrgKnn0txxq1cBlsl24WvsfB_Y0S395A_MylHJaQ5FSl7SEYDoE4*/ }
 const ModalContacts = ({ toggle, content }) => {
-  const [isShown, setIsShown] = useState(false);
-  const hide = () => setIsShown(false);
-  const show = () => setIsShown(true);
+  const [isShown, modalIsShown] = useState(false);
+  const hide = () => modalIsShown(false);
+  const show = () => modalIsShown(true);
 
   return (
     <>
